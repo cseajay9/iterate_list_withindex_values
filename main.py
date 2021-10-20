9f9a60ce8df75e841f20acf6684c660ad3ab8d5b
cseajay9 <cseajay9@gmail.com>
2021-10-20 20:35:40 +0545

    Add files via upload
    In this program, we have accessed the list via its values and index as well, using for loop

---
+++ main.py
@@ -0,0 +1,12 @@
+# iterating list with its values and index no.
+
+# iterating list to get list item
+our_list = ['apple', 'mango', 'banana', 'cherry', 'kiwi']
+for values in our_list:
+    print(values, " ")
+
+# iterating through list to get index no. of list
+fruit_list = ['apple', 'banana', 'orange', 'jackfruit', 'sabeda']
+for index in range(len(fruit_list)):
+    print("at index: ", index, "the list has: ", fruit_list[index])
+
